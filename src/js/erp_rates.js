9847d68b0cd636d57a7933c5ec682812def95441
nicodelasc <sychua.2021@scis.smu.edu.sg>
2022-11-10 17:23:33 +0800

    updated

--- src/js/erp_rates.js
+++ src/js/erp_rates.js
@@ -26,7 +26,10 @@ var app2 = Vue.createApp({
                     console.log(response.data[0].value[0].StartTime)
                     var start_time = response.data[0].value[0].StartTime
                     var end_time = response.data[0].value[0].EndTime
-                    
+                    var start_hour = Number(start_time.slice(0,2))
+                    var end_hour = Number(end_time.slice(0,2))
+                    var start_minute = Number(start_time.slice(3,5))
+                    var end_minute = Number(end_time.slice(3,5))
                     var total_rate = response.data[0].value[0].ChargeAmount
                     var date = new Date()
 	                var current_time = date.getHours()+":"+date.getMinutes()
@@ -34,8 +37,6 @@ var app2 = Vue.createApp({
                     var current_minute = date.getMinutes()
                     current_hour = Number(current_hour)
                     current_time = Number(current_minute)
-
-                    console.log(current_hour)
                     //current time returns string
                     console.log(typeof(current_time))
                     
