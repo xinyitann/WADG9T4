c6c4e8baf2ea9bc2334d895aa3fd2bcc71c8e9b9
Balveen Kaur <balveenkaur01@gmail.com>
2022-11-10 14:46:34 +0800

    car and index

--- src/js/carpark_avalibility.js
+++ src/js/carpark_avalibility.js
@@ -18,7 +18,7 @@ var app = Vue.createApp({
         carpark_avalibility() {
             console.log("yes")
 
-            let api_endpoint_url = '../../src/php/car/carpark_avalibility.php' 
+            let api_endpoint_url = '../src/php/car/carpark_avalibility.php' 
             axios.get(api_endpoint_url)
                 .then(response => {
                     console.log("SUCCESS")
